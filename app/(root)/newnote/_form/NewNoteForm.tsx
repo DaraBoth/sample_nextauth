3bf822c5e3807dcee2c1e6a41224c854e08fb899
GuoErr <43460012+DaraBoth@users.noreply.github.com>
2023-11-26 15:38:37 +0700

    fix abit

--- app/(root)/newnote/_form/NewNoteForm.tsx
+++ app/(root)/newnote/_form/NewNoteForm.tsx
@@ -7,6 +7,7 @@ const NewNoteForm = (props: any) => {
     const { userID } = props;
     const router = useRouter()
     const [member, setMember] = useState('');
+    const [isLoading, setIsLoading] = useState(0) // 0 false / 1 true / 2 error / 3 success
     const [memberList, setMemberList] = useState<string[]>([]);
     const handleCancel = () => {
         // Handle the cancel logic or navigate back
@@ -20,10 +21,17 @@ const NewNoteForm = (props: any) => {
     };
     return (
         <form action={async (formData: FormData) => {
+            setIsLoading(1)
             const title = formData.get("title") as string;
             const description = formData.get("description") as string;
             const user_id = userID;
-            createNote({ user_id, grp_name: title, description, status: 1, member: JSON.stringify(memberList) })
+            if (title && description && user_id) {
+                createNote({ user_id, grp_name: title, description, status: 1, member: JSON.stringify(memberList) })
+                setIsLoading(3)
+            } else {
+                alert("Please fill all fields.")
+                setIsLoading(0)
+            }
         }} className="flex flex-col md:flex-row">
             <div className="md:w-1/2 px-8 md:p-8">
                 <h2 className="text-2xl font-semibold mb-6">Create New Note</h2>
@@ -47,30 +55,32 @@ const NewNoteForm = (props: any) => {
                     <textarea
                         id="description"
                         name="description"
-                        className="w-full px-3 py-2 border rounded-md min-h-[25px] focus:outline-none focus:border-blue-500"
+                        className="w-full px-3 py-2 border rounded-md min-h-[40px] focus:outline-none focus:border-blue-500"
                         placeholder="Enter description"
                         rows={4}
                     />
                 </div>
-                <div className="hidden md:w-full md:flex justify-end">
-                    <button
-                        className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                    >
-                        Create Note
-                    </button>
 
+            </div>
+
+            <div className="md:w-1/2 px-8 md:p-8">
+                <div className="hidden md:w-full md:flex justify-end mb-[14px]">
                     <button
                         type="button"
-                        className="border ml-2 px-3 py-2 rounded-md hover:border-blue-500 focus:outline-none"
+                        className="border  px-3 py-2 rounded-md hover:border-blue-500 focus:outline-none"
                         onClick={handleCancel}
                     >
                         Cancel
                     </button>
+                    <button
+                        disabled={isLoading == 1}
+                        className="bg-blue-500 text-white px-3 py-2 ml-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                    >
+                        {isLoading == 0 && "Create Note"}
+                        {isLoading == 1 && "Creating Note..."}
+                        {isLoading == 3 && "Created"}
+                    </button>
                 </div>
-            </div>
-
-            <div className="md:w-1/2 px-8 md:p-8">
-
                 <div className="mb-4">
                     <h3 className="block text-gray-700 text-sm font-bold mb-2">Add Member</h3>
                     <div className="relative flex items-center">
@@ -85,7 +95,7 @@ const NewNoteForm = (props: any) => {
                         />
                         <button
                             type="button"
-                            className="ml-2 bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                            className="ml-2 border border-blue-700 px-3 py-2 rounded-md hover:border-blue-900 focus:outline-none"
                             onClick={handleAddMember}
                         >
                             <svg
@@ -93,7 +103,7 @@ const NewNoteForm = (props: any) => {
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 stroke="currentColor"
-                                className="h-5 w-5"
+                                className="h-5 w-5 text-blue-700"
                             >
                                 <path
                                     strokeLinecap="round"
@@ -110,19 +120,32 @@ const NewNoteForm = (props: any) => {
                     <label htmlFor="memberList" className="block text-gray-700 text-sm font-bold mb-2">
                         Members
                     </label>
-                    <ul className="list-none grid gap-2 grid-cols-2">
+                    <ul className="list-none grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                         {memberList.map((memberItem, index) => (
                             <li
                                 key={memberItem}
                                 className="bg-gray-200 p-2 border rounded-md flex justify-between items-center"
                             >
-                                {memberItem}
+                                <span className="truncate">{memberItem}</span>
                                 <button
                                     type="button"
                                     className="text-red-500 hover:text-red-700"
                                     onClick={() => setMemberList(memberList.filter((m) => m !== memberItem))}
                                 >
-                                    Remove
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        className="h-5 w-5"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M18 12H6"
+                                        />
+                                    </svg>
                                 </button>
                             </li>
                         ))}
@@ -130,9 +153,12 @@ const NewNoteForm = (props: any) => {
                 </div>
                 <div className="md:hidden w-full flex justify-end my-4 ">
                     <button
+                        disabled={isLoading == 1}
                         className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                     >
-                        Create Note
+                        {isLoading == 0 && "Create Note"}
+                        {isLoading == 1 && "Creating Note..."}
+                        {isLoading == 3 && "Created"}
                     </button>
                     <button
                         type="button"
